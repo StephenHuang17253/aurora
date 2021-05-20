495e06bfd2408f8225a8bb0ab12391bec119467b
StephenHuang17253 <57117612+StephenHuang17253@users.noreply.github.com>
2021-05-20 13:04:37 +1200

    First commit of a new dawn.

---
+++ main.py
@@ -0,0 +1,102 @@
+"""
+Stephen Huang
+03/05/21 - 
+Year 13 Flask webapp: Digital Reading Log/Journal
+"""
+
+import os
+from flask import Flask, render_template, session, redirect, url_for, request, Blueprint, flash
+from flask_sqlalchemy import SQLAlchemy
+from random import randint, choice
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "journal.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.secret_key = ("29fc9d808e2fa590040dc20e43d41c7346324bf9fe184273")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+import models
+
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+
+@app.route('/journal', methods=["GET", "POST"])
+def journal():
+    #book = Books.query.filter_by(title="Dune").first_or_404()
+    book = models.Books.query.all()
+    return render_template("journal.html", book=book, title=journal)
+
+
+@app.route('/update', methods=["GET", "POST"])
+def update():
+   return render_template("update.html")
+
+
+def current_user(): # current user function
+    if session.get("user"): # if it is able to get a session for user
+        return models.Users.query.get(session["user"]) # return the user info
+    else:
+        return False
+
+
+@app.context_processor
+def add_current_user():
+    if session.get('user'): # if a user is logged in
+        return dict(current_user=models.Users.query.get(session['user'])) # current user is equal to userinfo of user session
+    return dict(current_user=None) # otherwise current user is none
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if session.get("user"): # 
+        return redirect('/') # redirects to home page
+    if request.method == "POST": # request method 
+        User = models.Users.query.filter(models.Users.username==request.form.get("username")).first() # form fillable to gain username variable:
+        if User and check_password_hash(User.password, request.form.get("password")): # checks to see if the password is correct
+            flash("You have succesfully logged in") # tells the user they have succesfully logged in.
+            session['user']=User.userid
+            return redirect('/') # redirects to home page
+
+        else:
+            return render_template('login.html', error='Username exceeds limit of 20 characters or does not exist')
+    return render_template("login.html") # the html template for this is login.html
+
+
+@app.route('/logout') # /logout page
+def logout(): # logout function
+    try:
+        session.pop("user") # ends user session
+    except:
+        print('you are already logged out!') # if there is no session to pop this will be printed
+        return redirect("/login")
+    return redirect("/")
+
+
+@app.route('/signup', methods=["GET","POST"])
+def signup(): # create user function
+    if request.method == "POST":
+        if len(request.form.get('username')) > 20: # if the inputted username is greater than 20 characters it will not be accepted
+            return render_template('signup.html', error='Username exceeds limit of 20 characters') # prompts the user to create a shorter username
+        elif models.Users.query.filter(models.Users.username == request.form.get("username")).first():
+            return render_template('signup.html', error='Username already in use') # prompts the user to create a unique username
+        else:
+            user_info = models.Users (  
+                username = request.form.get('username'), # requests username from the user as a form
+                password = generate_password_hash(request.form.get('password'), salt_length=10), # requests password from the user as a form then salts and hashes it with a salt length of 10
+            )
+            db.session.add(user_info) # adds the data to the database
+            db.session.commit() # commits the add
+            flash("You have succesfully registed an Aurora account.") # tells the user they have succesfully logged in.
+    return render_template('signup.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,54 @@
+from main import db
+
+
+BooksAuthors = db.Table("BooksAuthors", db.Model.metadata,
+                db.Column("bookid", db.Integer, db.ForeignKey("Books.bookid")),
+                db.Column("authorid", db.Integer, db.ForeignKey("Authors.authorid")))
+
+
+BooksUsers = db.Table("BooksUsers", db.Model.metadata,
+                db.Column("bookid", db.Integer, db.ForeignKey("Books.bookid")),
+                db.Column("userid", db.Integer, db.ForeignKey("Users.userid")))
+
+
+BooksGenres = db.Table("BooksGenres", db.Model.metadata,
+                db.Column("bookid", db.Integer, db.ForeignKey("Books.bookid")),
+                db.Column("genreid", db.Integer, db.ForeignKey("Genres.genreid")))
+
+
+class Books (db.Model):
+    __tablename__ = "Books"
+    bookid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80)) 
+    sypnosis = db.Column(db.String(80)) 
+    year = db.Column(db.String(80)) 
+    
+    authors = db.relationship("Authors", secondary=BooksAuthors, back_populates="booksauthors")
+    users = db.relationship("Users", secondary=BooksUsers, back_populates="booksusers")  
+    genres = db.relationship("Genres", secondary=BooksGenres, back_populates="booksgenres")
+
+
+class Authors (db.Model):
+    __tablename__ = "Authors"
+    authorid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80)) 
+
+    booksauthors = db.relationship("Books", secondary=BooksAuthors, back_populates="authors")
+
+
+class Users (db.Model):
+    __tablename__ = "Users"
+    userid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))       
+    password = db.Column(db.String(80)) 
+
+    booksusers = db.relationship("Books", secondary=BooksUsers, back_populates="users")
+
+
+class Genres (db.Model):
+    __tablename__ = "Genres"
+    genreid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))       
+    description = db.Column(db.String(80)) 
+
+    booksgenres = db.relationship("Books", secondary=BooksGenres, back_populates="genres")
