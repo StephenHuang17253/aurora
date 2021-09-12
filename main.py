8c2e3aae5234e8418d9d2891d758862fcb562761
StephenHuang17253 <17253@burnside.school.nz>
2021-09-13 11:45:02 +1200

    0.63 - Add 404 page
    Probably should've been done ages ago.

--- main.py
+++ main.py
@@ -235,5 +235,10 @@ def signup(): # create account / register function
     return render_template('signup.html', page_title="Signup")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", page_title="404")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
